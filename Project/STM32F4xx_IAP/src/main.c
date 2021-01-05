3aa660d980ea69e5caa2a2514d862f07b0e4e521
Loly Law <loly.law@cygia.com>
2021-01-05 14:51:34 +0800

    initial commit

---
+++ Project/STM32F4xx_IAP/src/main.c
@@ -0,0 +1,160 @@
+/**
+  ******************************************************************************
+  * @file    STM32F4xx_IAP/src/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    10-October-2011
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */ 
+
+/** @addtogroup STM32F4xx_IAP
+  * @{
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "menu.h"
+#include "stm324xg_eval.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+extern pFunction Jump_To_Application;
+extern uint32_t JumpAddress;
+uint32_t Command_str;
+
+/* Private function prototypes -----------------------------------------------*/
+static void IAP_Init(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /* Unlock the Flash Program Erase controller */
+  FLASH_If_Init();
+
+  Command_str = *(__IO uint32_t*)0x08004C00;
+  /* Test if the app download flag set */
+  if((Command_str == 0xFFFFFFFF))
+  { 
+    /* Execute the IAP driver in order to reprogram the Flash */
+    IAP_Init();
+    /* Display main menu */
+    Main_Menu ();
+  }
+  /* Keep the user application running */
+  else
+  {
+    /* Test if user code is programmed starting from address "APPLICATION_ADDRESS" */
+    if (((*(__IO uint32_t*)APPLICATION_ADDRESS) & 0x2FF80000 ) == 0x20000000)
+    { 
+      /* Jump to user application */
+      JumpAddress = *(__IO uint32_t*) (APPLICATION_ADDRESS + 4);
+      Jump_To_Application = (pFunction) JumpAddress;
+      /* Initialize user application's Stack Pointer */
+      __set_MSP(*(__IO uint32_t*) APPLICATION_ADDRESS);
+      Jump_To_Application();
+    }
+  }
+
+  while (1)
+  {}
+}
+
+/**
+  * @brief  Initialize the IAP: Configure USART.
+  * @param  None
+  * @retval None
+  */
+void IAP_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  USART_InitTypeDef USART_InitStructure;
+
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  
+  /* Configure GPIO pins : DE_Pin RE_Pin for RS485 */
+  GPIO_InitStruct.GPIO_Pin = EVAL_COM1_DE_PIN
+                            | EVAL_COM1_RE_PIN
+                            | EVAL_COM1_RELAY2_PIN
+                            | EVAL_COM1_RELAY3_PIN
+                            | EVAL_COM1_RELAY4_PIN
+                            | EVAL_COM1_RELAY5_PIN;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  
+  GPIO_InitStruct.GPIO_Pin = EVAL_COM1_RELAY1_PIN;
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  GPIO_ResetBits(GPIOB, EVAL_COM1_RELAY1_PIN);
+  GPIO_ResetBits(GPIOA, EVAL_COM1_RELAY2_PIN);
+  GPIO_ResetBits(GPIOA, EVAL_COM1_RELAY3_PIN);
+  GPIO_ResetBits(GPIOA, EVAL_COM1_RELAY4_PIN);
+  GPIO_ResetBits(GPIOA, EVAL_COM1_RELAY5_PIN);
+
+  /* USART resources configuration (Clock, GPIO pins and USART registers) ----*/
+  /* USART configured as follow:
+        - BaudRate = 115200 baud  
+        - Word Length = 8 Bits
+        - One Stop Bit
+        - No parity
+        - Hardware flow control disabled (RTS and CTS signals)
+        - Receive and transmit enabled
+  */
+  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+  STM_EVAL_COMInit(COM1, &USART_InitStructure);
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
